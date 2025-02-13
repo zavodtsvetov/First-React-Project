f6b71c092c955274f695a62bf721d00e99d59eb4
zavodtsvetov <mitfahov@gmail.com>
2025-02-13 14:21:51 +0500

    final

--- my-app/vite-initial/src/App.jsx
+++ my-app/vite-initial/src/App.jsx
@@ -1,13 +1,12 @@
-import reactLogo from './assets/react.svg'
+import ReactLogo from './assets/react.svg?react'
+import reactLogo from './assets/react.svg';
 
 export const App = () => {
 	return (
 		<div className="App">
 			<p>Hello World</p>
-			<header className="App-header">
-				<img src={reactLogo} alt="React Logo"/>
-
-			</header>
+			<img src={reactLogo} alt="react Logo" />
+				<ReactLogo/>
 		</div>
 	);
 };
